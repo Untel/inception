6a219ebc75dc5306f1a73d949af04999c1775cd7
Adrien Fernandes <adrien@fernandes.bzh>
2022-04-22 09:31:10 +0200

    Finally make wordpress work

--- srcs/requirements/wordpress/tools/entry-point.sh
+++ srcs/requirements/wordpress/tools/entry-point.sh
@@ -17,20 +17,21 @@ if wp --path=/dev/null help "$1" > /dev/null 2>&1; then
 fi
 echo 'Executing'
 
+install_dir=/var/www
 
 # Execute aliases in the make file or directly the provided command
 if [ "$1" == "install" ] || [ "$1" == "configure" ]; then
-    if ! $(wp core is-installed --path="/var/www"); then
+    if ! $(wp core is-installed --path="$install_dir"); then
         echo 'DOWNLOADING WORDPRESS CLI'
-        wp core download --path=/var/www
+        wp core download --path=$install_dir
     fi
     echo 'CREATING WORDPRESS CONFIG'
-    wp config create --path="/var/www" --dbname=$WP_DB_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --dbcharset="utf8" --dbcollate="utf8_general_ci"
-    cat /var/www/wp-config.php
+    wp config create --path=$install_dir --dbname=$WP_DB_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --dbcharset="utf8" --dbcollate="utf8_general_ci"
+    cat $install_dir/wp-config.php
     echo 'CORE INSTALL WORDPRESS CONFIG'
-    wp core install --path="/var/www" --url=$DOMAIN_NAME/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL
+    wp core install --path=$install_dir --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL
     echo 'CREATING USER'
-    wp user create --path="/var/www" $WP_USER $WP_EMAIL --user_pass=$WP_PWD --role="editor"
+    wp user create --path=$install_dir $WP_USER $WP_EMAIL --user_pass=$WP_PWD --role="editor"
 else
   echo "WPCLI EXEC: $1"
   exec "$@"
