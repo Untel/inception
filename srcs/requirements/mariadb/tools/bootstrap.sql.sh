3ea4d4fbedbf3a12e69356deb824d23b63d5285e
Adrien Fernandes <adrien@fernandes.bzh>
2022-05-11 21:19:38 +0200

    Fix it

--- srcs/requirements/mariadb/tools/bootstrap.sql.sh
+++
@@ -1,8 +0,0 @@
-tfile=`mktemp`
-cat << EOF > $tfile
-    FLUSH PRIVILEGES;
-    CREATE DATABASE $WP_DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-    GRANT ALL ON $WP_DB_NAME.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-    FLUSH PRIVILEGES;
-EOF
-cat $tfile > /bootstrap.sql
